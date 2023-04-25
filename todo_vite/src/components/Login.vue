73128c965ce883f72253e1def021e85f857b74a1
Igor Parfenov <lloopy123123543@gmail.com>
2023-04-25 22:34:43 +0500

    Change login

--- todo_vite/src/components/Login.vue
+++ todo_vite/src/components/Login.vue
@@ -5,10 +5,12 @@
 <template>
   <div class="w-full h-max flex items-center justify-center">
     <div class="flex items-center justify-center w-1/2 bg-white h-screen">
-      <div class="flex flex-col gap-4 w-5/6 h-96 p-96 border-2 shadow-slate-800 bg-blue-100">
-        <div class="">ToDo</div>
-        <div class="">ToDo</div>
-        <div class="">ToDo</div>
+      <div class="flex flex-col gap-4 w-5/6 h-96 p-70 border-2 shadow-slate-800 bg-blue-100">
+        <div class="text-5xl text-center">ToDo</div>
+        <div class="">Login</div>
+        <input type="text" placeholder="Login" class="w-96">
+        <div class="">Password</div>
+        <input type="text" placeholder="Password" class="w-96">
         <div class="">ToDo</div>
       </div>
     </div>
