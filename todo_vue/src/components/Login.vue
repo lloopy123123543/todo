39b8c793547e00416b83dbc187f158518f877a59
Igor Parfenov <lloopy123123543@gmail.com>
2023-05-25 14:25:55 +0500

    fix

--- todo_vue/src/components/Login.vue
+++ todo_vue/src/components/Login.vue
@@ -37,7 +37,7 @@ function enter(){
     </div>
     <div>
       <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-      <input v-model="password" type="text"
+      <input v-model="password" type="password"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Password" required>
     </div>
